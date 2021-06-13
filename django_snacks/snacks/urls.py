ff0eac0f0022c8386eb2f15208f864ad07202131
reema-eilouti <reema.eilouti@gmail.com>
2021-06-13 21:22:21 +0300

    added templates

--- django_snacks/snacks/urls.py
+++ django_snacks/snacks/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+# from .views import HomePageView, AboutView
+
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name = 'index'),
+    path('home', views.HomePageView.as_view(), name = 'home'),
+    path('about', views.AboutView.as_view(), name = 'about'),
 ]
--- django_snacks/snacks/views.py
+++ django_snacks/snacks/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
 
 # Create your views here.
-from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the snacks index.")
+
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
