ea966df2fee85ad0277591bb595db18af6333a81
reema-eilouti <reema.eilouti@gmail.com>
2021-06-14 12:17:50 +0300

    fixed templates path

--- django_snacks/django_snacks/urls.py
+++ django_snacks/django_snacks/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('snacks/', include('snacks.urls')),
     path('admin/', admin.site.urls),
+    path('snacks/', include('snacks.urls')),
 ]
